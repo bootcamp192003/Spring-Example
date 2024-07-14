1b7dc8faff95d63fa62e3acfce9ef44922a7995d
Walter Javier <98858664+walterjav19@users.noreply.github.com>
2024-07-13 23:00:52 -0600

    Sucursal Añadida
    hay que agregar eso para una tabla mas

---
+++ src/main/java/com/laserants/spring_api/models/SucursalModel.java
@@ -0,0 +1,59 @@
+package com.laserants.spring_api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="sucursal")
+public class SucursalModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String direccion;
+	
+	@Column
+	private String telefono;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
